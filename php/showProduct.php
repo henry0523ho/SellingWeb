a012556497db254ee899b9f1000430617f374d50
Yu-Chien <00957035@email.ntou.edu.tw>
2022-11-23 23:54:41 +0800

    single.html & showProducr.php & .js

---
+++ php/showProduct.php
@@ -0,0 +1,24 @@
+<?php 
+require_once 'conn.php'; 
+
+$query = "SELECT * FROM product";
+$query_run = mysqli_query($conn,$query); 
+
+$outputData = array();
+
+if(mysqli_num_rows($query_run) > 0){
+    foreach($query_run as $row){
+        $obj = [
+            'url' => $row['product_img'],
+            'name' => $row['product_name'],
+            'price' => $row['product_cost'],
+        ];
+        //echo Json_encode($obj);
+        array_push($outputData, Json_encode($obj));        
+    }
+}
+
+$outputJson = json_encode($outputData);
+echo $outputJson;
+
+?>
---
+++ script/showProduct.js
@@ -0,0 +1,92 @@
+$(document).ready(function() {
+    showAllProduct();
+});
+
+
+function showAllProduct(){
+    $.ajax({
+        url: 'http://localhost/workSpace/SellingWeb/php/showProduct.php',
+        type: "POST",
+    
+        success: function(result) {
+            let string = "";
+            let objs = JSON.parse(result);
+            for(let i=0; i<objs.length; i++){
+                let objContext = JSON.parse(objs[i]);
+
+                let div01 = document.createElement("div");
+                div01.setAttribute('class', "col-md-4 text-center animate-box fadeInUp animated-fast");
+
+                let div02 = document.createElement("div");
+                div02.setAttribute("class", "product");
+
+                let div03 = document.createElement("div");
+                div03.setAttribute("class", "product-grid");
+                div03.style.backgroundImage = "url(\"images/product-1.jpg\")";
+
+                let div04 = document.createElement("div");
+                div04.setAttribute("class", "inner");
+
+                let div05 = document.createElement("div");
+                div05.setAttribute("class", "desc");
+
+                let p01 = document.createElement("p");
+                
+                let a01 = document.createElement("a");
+                a01.href = "single.html";
+                a01.setAttribute("class", "icon");
+
+                let i01 = document.createElement("i");
+                i01.setAttribute("class", "icon-shopping-cart");
+
+                a01.appendChild(i01);
+
+                let a02 = document.createElement("a");
+                a02.href = "single.html";
+                a02.setAttribute("class", "icon");
+                let i02 = document.createElement("i");
+                i02.setAttribute("class", "icon-eye");
+
+                a02.appendChild(i02);
+
+                let h301 = document.createElement("h3");
+        
+                let a03 = document.createElement("a");
+                a03.href = "single.html";
+                a03.appendChild(document.createTextNode("資工大二用書-自算機組織學"));
+                h301.appendChild(a03);
+
+
+                let s01 = document.createElement("span");
+                s01.setAttribute("class", "Price");
+                s01.appendChild(document.createTextNode("$150"));
+
+                div05.appendChild(h301);
+                div05.appendChild(s01);
+                p01.appendChild(a01);
+                p01.appendChild(a02);
+                div04.appendChild(p01);
+                div03.appendChild(div04);
+                div03.appendChild(div05);
+                div02.appendChild(div03);
+                div01.appendChild(div02);
+                
+                document.getElementById("showProduct").appendChild(div01);
+
+                // document.getElementById("showProduct").append(newDiv);
+
+                // string += '<div class="col-md-4 text-center animate-box"><div class="product"><div class="product-grid" style="background-image:url("';
+                // string += 'images/product-1.jpg';
+                // string += '");">';
+                // string += '<div class="inner"><p><a href="single.html" class="icon"><i class="icon-shopping-cart"></i></a><a href="single.html" class="icon"><i class="icon-eye"></i></a></p></div></div><div class="desc"><h3><a href="single.html">';
+                // string += objContext.name;
+                // string += '</a></h3><span class="price">$';
+                // string += objContext.price;
+                // string += '</span></div></div></div>';
+
+                //console.log(string);
+            }
+            //document.getElementById("showProduct").innerHTML = string;
+        },
+    });
+}
