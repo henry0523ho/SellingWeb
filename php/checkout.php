fd33865345d739c3b8e84c95bb0daa95e0a58219
chengen <chengenl20@gmail.com>
2022-11-25 22:05:01 +0800

    checkout
    成功結帳

--- php/checkout.php
+++ php/checkout.php
@@ -6,20 +6,22 @@ header("Access-Control-Allow-Origin: *");
 require_once "conn.php";
 $datas = $_POST; //預設 checkbox 傳進來，key = select = checkbox 的 name = id
 $outputData = array();
-foreach($datas as $row){
+foreach($datas as $key => $row){
     $sql = "SELECT * FROM `product` WHERE `product_id` = '{$row}'";
     $result = mysqli_query($conn, $sql);
     $product = mysqli_fetch_assoc($result);
+
     $sql2 = "SELECT * FROM `purchase` WHERE `product_id` = '{$product['product_id']}'";
     $result2 = mysqli_query($conn, $sql2);
-    $purchase = mysqli_fetch_assoc($result);
+    $purchase = mysqli_fetch_assoc($result2);
+
     $obj = [
        "id" => $product['product_id'],
        "name" =>  $product['product_name'],
-       "price" => $product['product_price'],
+       "price" => $product['product_cost'],
        "num" => $purchase['purchase_num'],
     ];
-    array_push($outputData, json_encode($obj));
+    array_push($outputData, $obj);
 }
 $outputData = json_encode($outputData);
 echo $outputData;
