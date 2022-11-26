da2594f906bc503c5f8cf524040359b5b775b6bc
chengen <chengenl20@gmail.com>
2022-11-26 09:04:01 +0800

    uploadProduct
    新增商品，缺圖片

---
+++ script/upload.js
@@ -0,0 +1,27 @@
+$(document).ready(function() { //上架商品
+    let conBtn = document.getElementById("conBtn");
+    conBtn.addEventListener("click", upload, false);
+});
+function upload(){
+    let productId = "";
+    let productName = $("#productName").val();
+    let productNum = $("#productNumber").val();
+    let productImg = "暫無圖片";
+    let productText = $("#productIntro").val();
+    let productLabel = $("#productCate").val();
+    let productNewRate = $("input[name=rating]:checked").val();
+    let productInfo = $("#productDescription").val();
+    let productCost = $("#productPrice").val();
+
+    $.ajax({
+        url: 'http://localhost/workSpace/SellingWeb/php/setProduct.php',
+        type: "POST",
+        data: { "productId": productId, "productName": productName, "productNum" : productNum,
+                "productImg" : productImg, "productText" : productText, "productLabel" : productLabel,
+                "productNewRate" : productNewRate, "productInfo" : productInfo, "productCost" : productCost},
+    })
+    .done(function(data){
+        let obj = JSON.parse(data);
+        console.log(obj.message);
+    })
+}
