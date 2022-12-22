cc5cf08e497d329e8c4dd402be262328957686f2
Kuan-Wei0316 <chen20020316@gmail.com>
2022-12-22 22:29:38 +0800

    wrong

---
+++ php/getProduct.php
@@ -0,0 +1,25 @@
+<?php
+require_once "conn.php";
+$inputData = $_POST;
+$outputData=array();
+try{
+    $productId=$_POST["productId"];
+    session_start();
+    if(isset($_SESSION["loggedin"]) && $_SESSION["loggedin"] === true)
+    {
+        $sql="SELECT * FROM product WHERE product_id = '" .$productId. "';";
+        $result=$conn->query($sql);
+        $row=$result->fetch_assoc();
+        $outputData['data']=array();
+        array_push($outputData['data'],$row);
+        $outputData['state']=200;
+        $outputData['message']="OK";
+    }
+    
+}catch(Exception $e){
+    $outputData['state']=500;
+    $outputData['message']=$e->getMessage();
+}
+$outputJson = json_encode($outputData);
+echo $outputJson;
+?>
